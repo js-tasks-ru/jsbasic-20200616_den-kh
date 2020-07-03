299daafadfdba1ff02e096351ca26a60ea9bf623
Denis <den.kh@mail.ru>
2020-07-03 17:57:38 +0300

    Решил задачи 1 в модулях 0 и 1

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -11,6 +11,7 @@ function print(text) {
  */
 function isValid(name) {
   // ваш код...
+  return !!name && !name.includes(' ') && name.length >= 4;
 }
 
 function sayHello() {
@@ -22,3 +23,4 @@ function sayHello() {
     print('Некорректное имя');
   }
 }
+
